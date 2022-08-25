e801d8304c5278462192da80bd664095fa9d49fd
Karoline-S <karoline.amy@gmail.com>
2022-08-25 06:18:05 +0000

    test - move chown command down

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,14 +19,14 @@ fi
 sudo mkdir --parents /data/web_static/shared/
 sudo mkdir --parents /data/web_static/releases/test/
 
-# set ownership and permissions of file structure
-sudo chown -R ubuntu:ubuntu /data/
-
 # create fake html file for testing
 sudo sh -c 'echo "It'\''s all working out for me" > /data/web_static/releases/test/index.html'
 
 # create symlink
 sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+
+# set ownership and permissions of file structure
+sudo chown -R ubuntu:ubuntu /data/
 
 # update nginx config to serve content of symlink to hbnb_static
 sudo sed -i '/listen \[::\]:80 default_server/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default

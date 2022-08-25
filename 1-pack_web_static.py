3b6ff3cc5c25c2b421f71104142cef88e5101875
Karoline-S <karoline.amy@gmail.com>
2022-08-25 09:16:41 +0000

    fix command string, add try block

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,19 +2,20 @@
 """create .tgz archive of web_static folder using Fabric
 """
 
-from fabric import operations
+from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
     """create a .tgz archive of web_static
     """
-    operations.local ('mkdir -p versions')
+
+    local('mkdir -p versions')
     now = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "./versions/web_static_" + now + ".tgz"
+    filename = "versions/web_static_" + now + ".tgz"
 
     try:
-        operations.local('tar -cvzf filename web_static')
+        local('tar -cvzf {} web_static'.format(filename))
         return filename
     except Exception:
         return None

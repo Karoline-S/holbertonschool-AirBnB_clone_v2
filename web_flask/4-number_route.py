61ad0cce643b7799098385a08deeb11247a45530
Karoline-S <karoline.amy@gmail.com>
2022-09-15 12:41:15 +0000

    build - add task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""starts a flask web app listenning on port 5000"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """returns a greeting when route is queried"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns hbnb for /hbnb query"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """displays url text on page"""
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is cool'):
+    """displays url text on page with default set"""
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_check(n):
+    """displays url text on page with default set"""
+    return str(n) + " is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

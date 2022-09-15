e2984b21b0274518614a25188b05a6fcf0c06157
Karoline-S <karoline.amy@gmail.com>
2022-09-15 12:12:57 +0000

    build - add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""starts a flask web app listenning on port 5000"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """returns a greeting when route is queried"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns hbnb for /hbnb query"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """displays url text on page"""
+    return "C " + text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

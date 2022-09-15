c9910e87834016cb095bb846f3edd4ef069ac227
Karoline-S <karoline.amy@gmail.com>
2022-09-15 12:01:34 +0000

    fix - pep8 styling

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hell0():
+    """returns a greeting when route is queried"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000)
+    app.run(host="0.0.0.0", port=5000)
